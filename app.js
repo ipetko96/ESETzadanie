e523adea2fa556dd87476d24484536a54eb16057
Ivan Peťko <ivanko.petko@gmail.com>
2021-08-23 09:53:11 +0200

    v2

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const port = 3000;
 app.get("/api/ivan", (req, res) => {
   res.send({
     hostname: os.hostname(),
-    uptime: process.uptime(),
+    uptime: msToHMS(process.uptime() * 1000),
   });
+  res.status(200)
 });
 
 app.listen(port, () => {
@@ -18,3 +19,10 @@ app.listen(port, () => {
 app.use("/healthcheck", require("express-healthcheck")());
 
 app.use(express.static("public"));
+
+function msToHMS(ms) {
+  let hours = Math.floor(ms / 3600000);
+  let minutes = Math.floor((ms % 3600000) / 60000)
+  let seconds = Math.floor(((ms % 3600000) % 60000) / 1000)
+  return minutes == 60 ? seconds + 1 + ":00" : seconds == 60 ? minutes + 1 + ":00" : ("0" + hours).slice(-2) + ":" + ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2);
+}
--- callTest.ps1
+++ callTest.ps1
@@ -1,5 +1,14 @@
+$ErrorActionPreference = "silentlycontinue"
+
 while ($true) {
-    $response = Invoke-RestMethod 'http://localhost:3000/api/ivan' -Method 'GET' -Headers $headers
-    "$($response.hostname) | $($response.uptime)"
+    $response = Invoke-RestMethod "http://20.93.203.74/api/ivan" -Method "GET" -Headers $headers -TimeoutSec 1 -StatusCodeVariable "statusCode" -ErrorVariable "badRequest"
+    if ($badRequest) {
+        Write-Host "!! ERROR !!" -BackgroundColor Red -ForegroundColor Black
+        continue
+    }
+    else {
+        Write-Host ($response.hostname, $response.uptime) -Separator " | " -BackgroundColor Green -ForegroundColor Black
+        Clear-Variable -Name "response"
+    }
     Start-Sleep -Seconds 1
 }
